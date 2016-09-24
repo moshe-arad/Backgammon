ee7d72f50df5926ec57196eb970dbfb029ab6476
moshe.arad <moshe.arad.mailbox@gmail.com>
2016-09-24 13:03:53 +0300

    adding persistence layer

--- backgammon-game-web/src/main/db/init.sql
+++ backgammon-game-web/src/main/db/init.sql
@@ -2,15 +2,20 @@ CREATE SCHEMA IF NOT EXISTS `backgammon`;
 USE `backgammon`;
 
 DROP TABLE IF EXISTS `game_user`;
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE `game_user`
+CREATE TABLE `users`
 (
-	`game_user_id` BIGINT NOT NULL AUTO_INCREMENT,
+	`user_id` BIGINT NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(255) NOT NULL,
     `last_name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `user_name` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `role` VARCHAR(255) NOT NULL,
-    PRIMARY KEY(`game_user_id`)
+    `last_updated_date` DATETIME NOT NULL,
+    `last_updated_by` BIGINT NOT NULL,
+    `created_date` DATETIME NOT NULL,
+    `created_by` BIGINT NOT NULL,
+    PRIMARY KEY(`user_id`)
 );
---
+++ backgammon-game-web/src/main/java/org/moshe/arad/data/entities/User.java
@@ -0,0 +1,135 @@
+package org.moshe.arad.data.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="user_id")
+	private Long userId;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	private String email;
+	
+	@Column(name="user_name")
+	private String userName;
+	
+	private String password;
+	
+	private String role;
+	
+	@Column(name="last_updated_date")
+	private Date lastUpdatedDate;
+	
+	@Column(name="last_updated_by")
+	private Long lastUpdatedBy;
+	
+	@Column(name="created_date")
+	private Date createdDate;
+	
+	@Column(name="created_by")
+	private Long createdBy;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Date getLastUpdatedDate() {
+		return lastUpdatedDate;
+	}
+
+	public void setLastUpdatedDate(Date lastUpdatedDate) {
+		this.lastUpdatedDate = lastUpdatedDate;
+	}
+
+	public Long getLastUpdatedBy() {
+		return lastUpdatedBy;
+	}
+
+	public void setLastUpdatedBy(Long lastUpdatedBy) {
+		this.lastUpdatedBy = lastUpdatedBy;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Long getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Long createdBy) {
+		this.createdBy = createdBy;
+	}
+}

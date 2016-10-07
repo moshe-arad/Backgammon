7c5a4569fb87379701bc34626846764c12a361e0
moshe.arad <moshe.arad.mailbox@gmail.com>
2016-10-07 05:34:13 +0300

    adding game room and user in game room tables to init.sql

--- backgammon-game-web/src/main/db/init.sql
+++ backgammon-game-web/src/main/db/init.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS `backgammon`;
 USE `backgammon`;
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `user_in_game_room`;
 DROP TABLE IF EXISTS `game_users`;
 
 CREATE TABLE `game_users`
@@ -20,4 +20,46 @@ CREATE TABLE `game_users`
     PRIMARY KEY(`user_id`)
 );
 
-INSERT INTO `game_users` (`first_name`, `last_name`, `email`, `user_name`, `password`, `role`, `last_updated_date`, `last_updated_by`, `created_date`, `created_by`) VALUES ('Moshe', 'Arad', 'moshe.arad.mailbox@gmail.com', 'moshe.arad', '1234', 'ROLE_USER', NOW(), 0, NOW(), 0);
+DROP TABLE IF EXISTS `game_rooms`;
+
+CREATE TABLE `game_rooms`
+(
+	`game_room_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `private` BIT(1) NOT NULL,
+    `white` BIGINT NOT NULL,
+    `black` BIGINT NOT NULL,
+    `opened_by` BIGINT NOT NULL,
+    `speed` BIT(3) NOT NULL,
+    `last_updated_date` DATETIME NOT NULL,
+    `last_updated_by` BIGINT NOT NULL,
+    `created_date` DATETIME NOT NULL,
+    `created_by` BIGINT NOT NULL,
+    PRIMARY KEY(`game_room_id`)
+);
+
+
+DROP TABLE IF EXISTS `user_in_game_room`;
+
+CREATE TABLE `user_in_game_room`
+(
+	`user_id` BIGINT NOT NULL,
+	`game_room_id` BIGINT NOT NULL,
+    `last_updated_date` DATETIME NOT NULL,
+    `last_updated_by` BIGINT NOT NULL,
+    `created_date` DATETIME NOT NULL,
+    `created_by` BIGINT NOT NULL,
+    PRIMARY KEY(`user_id`, `game_room_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `game_users`(`user_id`),
+    FOREIGN KEY (`game_room_id`) REFERENCES `game_rooms`(`game_room_id`)
+);
+
+
+
+
+
+
+
+
+
+

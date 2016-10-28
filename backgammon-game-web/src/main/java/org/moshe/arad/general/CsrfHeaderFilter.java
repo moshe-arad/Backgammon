0c02cc210e956e3600d6ffb367f1a1a571ffa23a
moshe-arad <moshe.arad.mailbox@gmail.com>
2016-10-28 05:48:29 +0300

    code fix

--- backgammon-game-web/src/main/java/org/moshe/arad/general/CsrfHeaderFilter.java
+++ backgammon-game-web/src/main/java/org/moshe/arad/general/CsrfHeaderFilter.java
@@ -23,16 +23,16 @@ public class CsrfHeaderFilter extends OncePerRequestFilter {
 	    CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
 	        .getName());
 	    if (csrf != null) {
-	    	Cookie cookie = new Cookie("_csrf", csrf.getToken());
-	        cookie.setPath("/");
-	        response.addCookie(cookie);
-//	      Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-//	      String token = csrf.getToken();
-//	      if (cookie==null || token!=null && !token.equals(cookie.getValue())) {
-//	        cookie = new Cookie("_csrf", token);
+//	    	Cookie cookie = new Cookie("_csrf", csrf.getToken());
 //	        cookie.setPath("/");
 //	        response.addCookie(cookie);
-//	      }
+	      Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+	      String token = csrf.getToken();
+	      if (cookie==null || token!=null && !token.equals(cookie.getValue())) {
+	        cookie = new Cookie("_csrf", token);
+	        cookie.setPath("/");
+	        response.addCookie(cookie);
+	      }
 	    }
 	    filterChain.doFilter(request, response);
 	  }
